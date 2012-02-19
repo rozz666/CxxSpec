91c4cc5eeb9b8335eb3d78b45743d6f953965ffd
Rafal Przywarski <rp@rp-desktop.(none)>
2012-02-19 10:17:20 +0100

    Moved ISpecificationVisitor to a new file

--- CxxSpec/ISpecificationVisitor.hpp
+++ CxxSpec/ISpecificationVisitor.hpp
@@ -25,46 +25,20 @@
 */
 
 
-#ifndef CXXSPEC_SPECIFICATION_HPP
-#define CXXSPEC_SPECIFICATION_HPP
-#include <CxxSpec/ISpecificationVisitor.hpp>
-
-#define CXXSPEC_CAT2(a, b) a##b
-#define CXXSPEC_CAT(a, b) CXXSPEC_CAT2(a, b)
-#define SPECIFICATION(desc) \
-    void CXXSPEC_CAT(CxxSpec__Specification_at_line_, __LINE__)(::CxxSpec::ISpecificationVisitor&); \
-    static int CXXSPEC_CAT(CxxSpec__Specification_register_at_line_, __LINE__) \
-        = (::CxxSpec::registerSpecification(desc, &CXXSPEC_CAT(CxxSpec__Specification_at_line_, __LINE__)), 0); \
-    void CXXSPEC_CAT(CxxSpec__Specification_at_line_, __LINE__)(::CxxSpec::ISpecificationVisitor& CxxSpec_specificationVisitor)
-
-#define SECTION(desc) \
-    if (const auto& CxxSpec_sectionGuard = ::CxxSpec::SectionGuard(CxxSpec_specificationVisitor, desc))
+#ifndef CXXSPEC_ISPECIFICATIONVISITOR_HPP
+#define CXXSPEC_ISPECIFICATIONVISITOR_HPP
+#include <string>
 
 namespace CxxSpec {
 
-class SectionGuard
+class ISpecificationVisitor
 {
 public:
-    explicit SectionGuard(ISpecificationVisitor& sv, const std::string& desc)
-        : sv(sv)
-    {
-        stepIn = sv.beginSection(desc);
-    }
-
-    ~SectionGuard()
-    {
-        sv.endSection();
-    }
-
-    virtual operator bool() const { return stepIn; }
-
-private:
-    bool stepIn;
-    ISpecificationVisitor& sv;
+    virtual ~ISpecificationVisitor() { }
+    virtual bool beginSection(const std::string& desc) = 0;
+    virtual void endSection() = 0;
 };
-
-typedef void (*SpecFunction)(ISpecificationVisitor&);
 
 }
 
-#endif // CXXSPEC_SPECIFICATION_HPP
+#endif // CXXSPEC_ISPECIFICATIONVISITOR_HPP
