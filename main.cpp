2e8117ae55fab1ea55911686450d9e8ffa6e82bd
Rafal Przywarski <rp@rp-desktop.(none)>
2012-02-21 20:44:06 +0100

    Specification registry

--- main.cpp
+++ main.cpp
@@ -2,9 +2,14 @@
 #include "testSpecification.hpp"
 #include "testExecutor.hpp"
 #include "testAssertions.hpp"
+#include "testSpecificationRegistry.hpp"
+#include <CxxSpec/CxxSpec.hpp>
 
 int main(int argc, char **argv) {
     testSpecification();
     testExecutor();
     testAssertions();
+    testSpecificationRegistry();
+
+    CxxSpec::SpecificationRegistry::getInstance().runAll();
 }
