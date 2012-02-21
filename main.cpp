80494804261ca24d8507e7bc83cd428d4148d985
Rafal Przywarski <rp@rp-desktop.(none)>
2012-02-21 19:16:13 +0100

    Assertions

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "testSpecification.hpp"
 #include "testExecutor.hpp"
+#include "testAssertions.hpp"
 
 int main(int argc, char **argv) {
     testSpecification();
     testExecutor();
+    testAssertions();
 }
