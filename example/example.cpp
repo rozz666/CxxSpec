619ff7f93a42fa51f0c7aae32c979a4f1027786c
Rafal Przywarski <rp@rp-desktop.(none)>
2012-11-10 11:50:15 +0100

    added std::vector<int> example

--- example/example.cpp
+++ example/example.cpp
@@ -11,3 +11,29 @@ describe("int")
         expect(5 == 5).should.beTrue();
     }
 }
+
+describe("std::vector<int>")
+{
+    std::vector<int> v;
+
+    it("should be empty when created")
+    {
+        expect(v.empty()).should == true;
+    }
+
+    it("should have size equal to the number of pushed elements")
+    {
+        v.push_back(7);
+        expect(v.size()).should == 1u;
+        v.push_back(8);
+        expect(v.size()).should == 2u;
+    }
+
+    it("pop_back should decrease size")
+    {
+        v.push_back(6);
+        v.push_back(8);
+        v.pop_back();
+        expect(v.size()).should == 1u;
+    }
+}
